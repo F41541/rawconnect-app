33ea471d246ec441a84fae61d774680150966876
Faisal <mfaisalfahri02@gmail.com>
2025-08-04 22:38:11 +0700

    Mengubah Tampilan Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
     <form method="POST" action="{{ route('login') }}">
         @csrf
         <div class="mb-3">
-            <label for="email" class="form-label">Ussername</label>
+            <label for="email" class="form-label">Email</label>
             <input id="email" type="email" name="email" class="form-control @error('email') is-invalid @enderror" value="{{ old('email') }}" required autofocus>
             @error('email')
                 <div class="invalid-feedback">{{ $message }}</div>
